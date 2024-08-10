04f8004e9f4b2c73a08c8bc58b4a57414c520266
igroks <igorcarvalhodasilva012@gmail.com>
2024-08-10 17:11:44 -0400

    docs: add swagger configs

---
+++ src/core/config/swagger/swagger.config.ts
@@ -0,0 +1,22 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { App } from 'src/core/interfaces/config.interface';
+
+export class SwaggerConfig {
+  static init(app: INestApplication) {
+    const configService = app.get(ConfigService);
+    const { name, version, company } = configService.get<App>('config.app');
+
+    const config = new DocumentBuilder()
+      .setTitle(name)
+      .setVersion(version)
+      .setLicense(`${company.name} © All rights reserved`, company.website)
+      .setDescription(`This is the documentation for ${name}`)
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerConfig } from './core/config/swagger/swagger.config';
 import { App } from './core/interfaces/config.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const { port } = configService.get<App>('config.app');
+  SwaggerConfig.init(app);
 
   app.useGlobalPipes(
     new ValidationPipe({
